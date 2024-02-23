b1d61590f00fff5a7aac091e5ccab872f7ea83dd
Amandeep4567 <amankumaradk@gmail.com>
2024-02-23 19:26:34 +0530

    eslint updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,10 @@ module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['simple-import-sort', 'prettier'],
+  plugins: ['prettier'],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'no-console': 'off',
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
   },
   parserOptions: {
     sourceType: 'module',
